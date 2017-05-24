7d42ae90ba1491dc996534e038f9eafb47c134f1
Lachezar Balev <l_@mail.bg>
2017-05-24 12:30:18 +0300

    Adding the zuul gateway

---
+++ api-gateway-service/src/main/java/de/rewe/demo/registry/Application.java
@@ -0,0 +1,17 @@
+package de.rewe.demo.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * The main application class for the registration service.
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
