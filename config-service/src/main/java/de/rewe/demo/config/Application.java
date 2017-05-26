05b9494b1e1f7b9d1dda08c6e6d6d702b4e07922
Lachezar Balev <l_@mail.bg>
2017-05-26 16:24:33 +0300

    Adding a new config service

---
+++ config-service/src/main/java/de/rewe/demo/config/Application.java
@@ -0,0 +1,19 @@
+package de.rewe.demo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * The main application class for the registration service.
+ */
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
