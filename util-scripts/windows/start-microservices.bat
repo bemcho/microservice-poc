d9e4c7a2a141b446f23459a5ab4ecbcaa5ef7c0f
Lachezar Balev <l_@mail.bg>
2017-05-26 16:25:20 +0300

    Adding the configuration service

--- util-scripts/windows/start-microservices.bat
+++ util-scripts/windows/start-microservices.bat
@@ -5,10 +5,11 @@ REM print menu
 echo Select which service to start:
 echo. 
 echo [1] Eureka Registry 
-echo [2] UAA Service
-echo [3] Product Service
-echo [4] Zuul Gateway
-echo [5] Quit
+echo [2] Configuration Service
+echo [3] UAA Service
+echo [4] Product Service
+echo [5] Zuul Gateway
+echo [6] Quit
 echo.
 REM EO MENU
 
@@ -18,13 +19,15 @@ set /p service_no="Choose a service:"
 
 IF %service_no%==1 (
 	set service_id=registry-service
-) ELSE IF %service_no%==2 (
-	set service_id=uaa-service
+) ELSE IF %service_no%==2 (	
+	set service_id=config-service
 ) ELSE IF %service_no%==3 (
-	set service_id=product-service
+	set service_id=uaa-service
 ) ELSE IF %service_no%==4 (
-	set service_id=api-gateway-service
+	set service_id=product-service
 ) ELSE IF %service_no%==5 (
+	set service_id=api-gateway-service
+) ELSE IF %service_no%==6 (
 	GOTO EXIT
 ) ELSE (
 	GOTO BAD_INPUT
