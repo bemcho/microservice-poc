1c40cdc78e1e7095bfe780b116b15fc2583a7ad5
Lachezar Balev <l_@mail.bg>
2017-05-26 16:31:27 +0300

    Integrating the configuration serive with the product service

--- product-service/src/main/java/de/rewe/demo/product/Application.java
+++ product-service/src/main/java/de/rewe/demo/product/Application.java
@@ -3,12 +3,10 @@ package de.rewe.demo.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * The main application class for the demo project.
  */
-@ComponentScan
 @SpringBootApplication
 @EnableEurekaClient
 public class Application {
