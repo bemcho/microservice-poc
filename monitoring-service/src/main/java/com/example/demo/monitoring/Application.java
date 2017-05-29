ae40153be1196192d1b737cc76f66dcef9121aa8
Lachezar Balev <l_@mail.bg>
2017-05-29 10:09:45 +0300

    Small refactoring

--- monitoring-service/src/main/java/com/example/demo/monitoring/Application.java
+++ monitoring-service/src/main/java/com/example/demo/monitoring/Application.java
@@ -1,4 +1,4 @@
-package de.rewe.demo.monitoring;
+package com.example.demo.monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
