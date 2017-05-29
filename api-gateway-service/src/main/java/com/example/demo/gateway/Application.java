fd7b3f2545a9e57c6a78d3ce47bacfff1e1d1570
Lachezar Balev <l_@mail.bg>
2017-05-29 10:08:06 +0300

    Small refactoring

--- api-gateway-service/src/main/java/com/example/demo/gateway/Application.java
+++ api-gateway-service/src/main/java/com/example/demo/gateway/Application.java
@@ -1,4 +1,4 @@
-package de.rewe.demo.registry;
+package com.example.demo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
