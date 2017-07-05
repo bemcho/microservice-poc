5373c69839244d90123e8d104c96ba4b7c3fae56
Martin Bonev <bonev.martin@gmail.com>
2017-07-05 12:33:35 +0300

    descriptions

--- promotion-service/src/main/resources/data.sql
+++ promotion-service/src/main/resources/data.sql
@@ -1,8 +1,9 @@
-Insert into Promotion(product_id, discount, description) values (1, 5, '');
-Insert into Promotion(product_id, discount, description) values (2, 5, '');
-Insert into Promotion(product_id, discount, description) values (3, 10, '');
-Insert into Promotion(product_id, discount, description) values (4, 10, '');
-Insert into Promotion(product_id, discount, description, card_number) values (5, 15, '', '9981011579550');
-Insert into Promotion(product_id, discount, description, card_number) values (5, 20, '', '9981012546452');
-Insert into Promotion(product_id, discount, description, card_number) values (6, 15, '', '9981011579550');
-Insert into Promotion(product_id, discount, description, card_number) values (7, 20, '', '9981012546452');
+Insert into Promotion(product_id, discount, description) values (1, 10, 'Fruit week: -10% on seasonal fruits.');
+Insert into Promotion(product_id, discount, description) values (2, 15, 'Local Vegetables Month: -15% on local vegetables');
+Insert into Promotion(product_id, discount, description) values (3, 12, 'Tomato Day: -12% on Cherry tomatoes');
+Insert into Promotion(product_id, discount, description) values (4, 5, 'Eat apples challenge: -5% on Braeburn apples.');
+
+Insert into Promotion(product_id, discount, description, card_number) values (5, 35, 'Stay young: -35% on Zucchini gruen.', '9981011579550');
+Insert into Promotion(product_id, discount, description, card_number) values (5, 20, 'Stay young: -20% on Zucchini gruen.', '9981012546452');
+Insert into Promotion(product_id, discount, description, card_number) values (6, 18, 'Exotic fruits: -18% on Kiwi.', '9981011579550');
+Insert into Promotion(product_id, discount, description, card_number) values (7, 20, 'Pear fanatic: -20% on Van Nahmen Williams Christ Birnensaft 0,75l', '9981012546452');
